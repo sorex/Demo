c3f8e78ad99c235017d3851ac105fc0b44daf8a3
sorex <sorex1984@gmail.com>
2013-11-12 23:24:34 +0800

    创建项目

---
+++ J.MainWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,102 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace J.MainWeb
+{
+	public class BundleConfig
+	{
+		// 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			#region CSS
+			#region bootstrap
+			bundles.Add(new StyleBundle("~/Static/Resources/bootstrap/css").Include(
+						"~/Static/Resources/bootstrap/bootstrap.css",
+						"~/Static/Resources/bootstrap/bootstrap-theme.css"));
+			#endregion
+
+			#region uploadify CSS
+			bundles.Add(new StyleBundle("~/Static/Resources/uploadify/css").Include(
+						"~/Static/Resources/uploadify/uploadify.css"));
+			#endregion
+
+			#region site CSS
+			bundles.Add(new StyleBundle("~/Static/Resources/site/css").Include(
+						"~/Static/Resources/site/site.css"));
+			#endregion
+			#endregion
+
+			#region JS
+			#region html5shiv
+			bundles.Add(new ScriptBundle("~/Static/Resources/html5shiv/js").Include(
+						"~/Static/Resources/html5shiv/html5shiv.js"));
+			#endregion
+
+			#region respond
+			bundles.Add(new ScriptBundle("~/Static/Resources/respond/js").Include(
+						"~/Static/Resources/respond/respond.js"));
+			#endregion
+
+			#region jquery-1.x
+			bundles.Add(new ScriptBundle("~/Static/Resources/jquery-1.x/js").Include(
+						"~/Static/Resources/jquery-1.x/jquery-{version}.js"));
+			#endregion
+
+			#region jquery-2.x
+			bundles.Add(new ScriptBundle("~/Static/Resources/jquery-2.x/js").Include(
+						"~/Static/Resources/jquery-2.x/jquery-{version}.js"));
+			#endregion
+
+			#region Modernizr
+			// 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
+			// 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
+			bundles.Add(new ScriptBundle("~/Static/Resources/modernizr/js").Include(
+						"~/Static/Resources/modernizr/modernizr-*"));
+			#endregion
+
+			#region bootstrap
+			bundles.Add(new ScriptBundle("~/Static/Resources/bootstrap/js").Include(
+						"~/Static/Resources/bootstrap/bootstrap.js"));
+			#endregion
+
+			#region bootstrap-datepicker
+			bundles.Add(new ScriptBundle("~/Static/Resources/bootstrap-datepicker/js").Include(
+						"~/Static/Resources/bootstrap-datepicker/bootstrap-datepicker.js",
+						"~/Static/Resources/bootstrap-datepicker/bootstrap-datepicker.zh-CN.js"));
+			#endregion
+
+			#region jquery validate
+			bundles.Add(new ScriptBundle("~/Static/Resources/jquery.validate/js").Include(
+						"~/Static/Resources/jquery.validate/jquery.validate.js",
+						"~/Static/Resources/jquery.validate/additional-methods.js",
+						"~/Static/Resources/jquery.validate/messages_zh.js"));
+			#endregion
+
+			#region mustache
+			bundles.Add(new ScriptBundle("~/Static/Resources/mustache/js").Include(
+						"~/Static/Resources/mustache/mustache.js"));
+			#endregion
+
+			#region json2
+			bundles.Add(new ScriptBundle("~/Static/Resources/json2/js").Include(
+						"~/Static/Resources/json2/json2.js"));
+			#endregion
+
+			#region uploadify
+			bundles.Add(new ScriptBundle("~/Static/Resources/uploadify/js").Include(
+						"~/Static/Resources/uploadify/jquery.uploadify.js"));
+			#endregion
+
+			#region jquery.base64
+			bundles.Add(new ScriptBundle("~/Static/Resources/jquery.base64/js").Include(
+						"~/Static/Resources/jquery.base64/jquery.base64.js"));
+			#endregion
+
+			#region extendjs
+			bundles.Add(new ScriptBundle("~/Static/Resources/extendjs/js").Include(
+						"~/Static/Resources/extendjs/extendjs.js"));
+			#endregion
+			#endregion
+		}
+	}
+}
---
+++ J.MainWeb/Static/Resources/extendjs/extendjs.js
@@ -0,0 +1,205 @@
+﻿/**********************************************************
+ * 模    块	：	通用js扩展类
+ * 作    者	：	Jasper
+ * 编写时间	：	2012-10-22
+ * 实现功能	：	添加通用方法，扩展String对象，Array对象，添加jquery扩展的render，initTemplate
+ * 详细描述	：	
+ * *********************************************************/
+
+
+//┏━━━━━━━━━━━━━━━━┓
+//┃ String对象的扩展  ┃
+//┗━━━━━━━━━━━━━━━━┛
+String.format = function ()
+{
+	/// <summary>
+	/// 将指定字符串s中的{0}，{1}。。。替换为指定字符串
+	/// </summary>
+	/// <returns type="String" />
+
+	var s = arguments[0];
+	for (var i = 0; i < arguments.length - 1; i++)
+	{
+		var reg = new RegExp("\\{" + i + "\\}", "gm");
+		s = s.replace(reg, arguments[i + 1]);
+	}
+
+	return s;
+};
+
+String.prototype.replace2 = function (oldStr, newStr)
+{
+	/// <summary>
+	/// 将字符串中的newStr替换为oldStr
+	/// </summary>
+	/// <returns type="String" />
+	var reg = new RegExp(oldStr, "gm");
+	return this.replace(reg, newStr);
+}
+
+String.prototype.trim = function ()
+{
+	/// <summary>
+	/// 从当前 String 对象移除所有前导空白字符和尾部空白字符。
+	/// &#10;将会返回一个新字符串
+	/// </summary>
+	/// <returns type="String" />
+	return this.replace(/(^\s*)|(\s*$)|\r|\n/g, "");
+}
+
+String.prototype.trimLeft = function ()
+{
+	/// <summary>
+	/// 从当前 String 对象移除所有前导空白字符。
+	/// &#10;将会返回一个新字符串
+	/// </summary>
+	/// <returns type="String" />
+	return this.replace(/(^\s*)|\r|\n/g, "");
+};
+
+String.prototype.trimRight = function ()
+{
+	/// <summary>
+	/// 从当前 String 对象移除所有尾部空白字符。
+	/// &#10;将会返回一个新字符串
+	/// </summary>
+	/// <returns type="String" />
+	return this.replace(/(\s*$)|\r|\n/g, "");
+};
+
+String.prototype.endsWith = function (str)
+{
+	/// <summary>
+	/// 是否以指定字符串结尾
+	/// </summary>
+	/// <returns type="BOOL" />
+	return (this.substr(this.length - str.length) === str);
+};
+
+String.prototype.startsWith = function (str)
+{
+	/// <summary>
+	/// 是否以指定字符串开始
+	/// </summary>
+	/// <returns type="BOOL" />
+	return (this.substr(0, str.length) === str);
+};
+
+String.prototype.isNumber = function ()
+{
+	/// <summary>
+	/// 是否是数字
+	/// </summary>
+	/// <returns type="BOOL" />
+	return !isNaN(this);
+};
+
+String.prototype.isEmail = function ()
+{
+	/// <summary>
+	/// 是否是邮件地址
+	/// </summary>
+	/// <returns type="BOOL" />
+	return (new RegExp(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/).test(this.trim()));
+};
+
+String.prototype.encodeURI = function ()
+{
+	var returnString; returnString = escape(this)
+	returnString = returnString.replace(/\+/g, "%2B");
+	return returnString
+}
+
+String.prototype.decodeURI = function ()
+{
+	return unescape(this)
+}
+
+//┏━━━━━━━━━━━━━━━━┓
+//┃ float对象的扩展   ┃
+//┗━━━━━━━━━━━━━━━━┛
+RetainDigits = function (num, digits)
+{
+	/// <summary>
+	/// 将指定float数字保留几位小数
+	/// </summary>
+	/// <returns type="String" />
+
+	var result = num.toString();
+	var pos_decimal = result.indexOf('.');
+	if (pos_decimal < 0)
+	{
+		pos_decimal = result.length;
+		result += '.';
+	}
+	while (result.length <= pos_decimal + digits)
+	{
+		result += '0';
+	}
+	return result;
+};
+
+//┏━━━━━━━━━━━━━━━━┓
+//┃ jQuery对象的扩展  ┃
+//┗━━━━━━━━━━━━━━━━┛
+jQuery.fn.extend({
+	render: function (data)
+	{
+		this.data("template_data", $.extend({}, data, this.data("template_extend")));
+		this.html(Mustache.render(this.data("template"), this.data("template_data")));
+	}
+});
+
+jQuery.extend({
+	initTemplate: function ()
+	{
+		$(".template").each(function ()
+		{
+			var $this = $(this);
+			$this.data("template", $("script[type='text/x-mustache']",$this).text());
+			$this.data("template_extend", {});
+			$this.html("");
+		});
+	}
+});
+
+//┏━━━━━━━━━━━━━━━━━━┓
+//┃ 只能输入数字的input  ┃
+//┗━━━━━━━━━━━━━━━━━━┛
+function onlyNumber(obj, event)
+{
+	//IE的onkeypress和onblur会同时触发
+	try
+	{
+		e = window.event || event;		//FF使用event，IE使用window.event，chrome使用window.event
+		var k = e.keyCode || e.which;	//FF使用which，IE使用keyCode，chrome使用IE使用keyCode
+		if ((k < 48 || k > 57) && !(obj.value.indexOf(".") == -1 && event.keyCode == 46))
+			if (e.preventDefault)
+				e.preventDefault();
+			else
+				e.returnValue = false;
+	}
+	catch (e)
+	{
+		alert(e.description);
+	}
+}
+
+function onlyInt(obj, event)
+{
+	//IE的onkeypress和onblur会同时触发
+	try
+	{
+		e = window.event || event;		//FF使用event，IE使用window.event，chrome使用window.event
+		var k = e.keyCode || e.which;	//FF使用which，IE使用keyCode，chrome使用IE使用keyCode
+		if (k < 48 || k > 57)
+			if (e.preventDefault)
+				e.preventDefault();
+			else
+				e.returnValue = false;
+	}
+	catch (e)
+	{
+		alert(e.description);
+	}
+}
